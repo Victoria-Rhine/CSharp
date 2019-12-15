82d64e8b12b63df2e42ce95145115112c58ffd73
Victoria Rhine <vrhine18@mail.wou.edu>
2019-12-15 12:30:16 -0800

    Add files via upload

---
+++ AdventofCodeDay1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AdventofCodeDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\torir\source\repos\AdventofCodeDay1\input.txt");
+
+            CalculateFuel(lines);
+
+            static void CalculateFuel(string[] lines)
+            {
+                double num;
+                double total = 0;
+
+                foreach (string line in lines)
+                {
+                    num = (Math.Floor(((Convert.ToDouble(line))) / 3) - 2);
+                    total += num;
+                }
+
+                Console.WriteLine("The total is: " + total);
+            }
+
+            CalculateTotalFuel(lines);
+
+            static void CalculateTotalFuel(string[] lines)
+            {
+                double num;
+                double total = 0;
+
+                foreach (string line in lines)
+                {
+                    num = (Math.Floor(((Convert.ToDouble(line))) / 3) - 2);
+                    total += num;
+
+                    if (Math.Floor(num / 3) - 2 > 0)
+                    {
+                        do
+                        {
+                           num = Math.Floor(num / 3) - 2;
+                           total += num;
+                        } while (Math.Floor(num / 3) - 2 > 0);
+                    }       
+                }
+                Console.WriteLine("The final total is: " + total);
+            }
+        }
+    }
+}
---
+++ AdventofCodeDay2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventofCodeDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\torir\source\repos\AdventofCodeDay2\input.txt");
+            string[] intcode = text.Split(',');
+
+            List<int> programNum = new List<int>();
+            foreach (var num in intcode) {
+                programNum.Add(Convert.ToInt32(num));
+            }
+
+            int opcode;
+            int value;
+            int first;
+            int second;
+            int location;
+
+           programNum[1] = 57; 
+           programNum[2] = 41;
+            //19690720
+
+            for (int i = 0; i <= programNum.Count; i += 4)
+            {
+                opcode = programNum[i];
+
+                if (opcode == 1)
+                {
+                    first = programNum[i + 1];
+                    second = programNum[i + 2];
+                    location = programNum[i + 3];
+                    value = programNum[first] + programNum[second];
+                    programNum[location] = value;
+                }
+                else if (opcode == 2)
+                {
+                    first = programNum[i + 1];
+                    second = programNum[i + 2];
+                    location = programNum[i + 3];
+                    value = programNum[first] * programNum[second];
+                    programNum[location] = value;
+                }
+                else if (opcode == 99)
+                            break;
+                else
+                    Console.WriteLine("Oops, unknown code");
+            }
+            Console.WriteLine("Value at position 0 is: " + programNum[0]);
+        }
+    }
+}
